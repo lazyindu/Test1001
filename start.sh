bdd7d8e81042d905c64d8805908776b6a87800b6
indu <70087467+lazyindu@users.noreply.github.com>
2022-12-11 14:55:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Your_Repo_Name /LazyDeveloper
+git clone https://github.com/lazyindu/Test1001 /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python3 main.py
