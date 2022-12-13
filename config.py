9c7530572c084c20d63a927868c94c8583e8be12
indu <70087467+lazyindu@users.noreply.github.com>
2022-12-13 22:01:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,25 +5,25 @@ from logging.handlers import RotatingFileHandler
 
 
 # Get a bot token from botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5965300871:AAFAYEp935IRFdnvBtu0A-h9fQ0vo_29VKo")
 
 # Get from my.telegram.org (or @UseTGXBot)
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "13323016"))
 
 # Get from my.telegram.org (or @UseTGXBot)
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "68e791e616100248b0a53ae86a661a12")
 
 # Generate a user session string 
-TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
+TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQC0YCEwGJETIwjc_Zx9HdbK1GNYDr07i3SSk11KYhMjcr8KmLwQnslkZOy9Am50QU4R7VO0IDcaiIesZOD53GuJ6It66i70WPZuX9zIdVwxAttro2nlGdlu8ZJD9F0wBbrObdgzUbwFqawW-fPpEOPQvfxnY2zSmE-yCeuKfEkV2GNE-al3f4hYqpfA5plfDrw2RyCPwYzZoxaJtHkUNWoVG3-djM95lqnyZ8zy8K2OeCo4-AW4zSn2sh5H2uj9Z3zGE6UqWUnANgMBzH78YXvyCVkrXJiw8pIfL9HMU1qLUb9iFYO6nyx5B6g9Q77Wt17TIF-BsFEUZP-CIufruHBgWH5FTAA")
 
 # Database URL from https://cloud.mongodb.com/
-DATABASE_URI = os.environ.get("DATABASE_URI", "")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://test:test@cluster0.krokuyo.mongodb.net/?retryWrites=true&w=majority")
 
 # Your database name from mongoDB
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 # ID of users that can use the bot commands
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1484670284").split())
 
 # Should bot search for document files in channels
 DOC_SEARCH = os.environ.get("DOC_SEARCH", "yes").lower()
